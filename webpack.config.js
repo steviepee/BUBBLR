33f72c02ae888f59b734746fddbfc7858a06f799
Hackman78 <pathenry231@gmail.com>
2024-03-14 19:59:11 -0500

    Set up webpack babel eslint src folders
    This reverts commit 05f9a1559b7147e5260a35d8b1f75c482eb0e101.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  resolve: {
+    extensions: [
+      '.jsx',
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
