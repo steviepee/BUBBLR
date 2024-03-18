7e54bada2f67142ec1fda63f290c8ae15cd86857
peytono <pcopcoeco@gmail.com>
2024-03-18 18:04:09 -0500

    add UserFriends table

--- server/db/index.js
+++ server/db/index.js
@@ -21,6 +21,28 @@ const User = sequelize.define('User', {
 User.sync()
   .catch((err) => console.error(err));
 
+
+const UserFriends = sequelize.define('UserFriends', {
+  friend1Id : {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    }
+  },
+  friend2Id : {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    }
+  }
+})
+
+UserFriends.sync()
+  .catch((err) => console.error('Failed syncing UserFriends: ', err));
+
 module.exports = {
   User,
+  UserFriends
 };
