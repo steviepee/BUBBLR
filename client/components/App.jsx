4a0e68582d8492f7fd9022307d68e4c1fd965bb8
peytono <pcopcoeco@gmail.com>
2024-03-17 19:30:39 -0500

    add basic profile component and fix eslint errors

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Profile from './Profile';
 
 function App() {
-  return <h1>KPCP Hotel</h1>;
+  return (
+    <h1>
+      KPCP Hotel
+      <Profile />
+    </h1>
+  );
 }
 
 export default App;
