3bcbd63bd114a423b77f2a566c43bc9a071fc9a6
peytono <pcopcoeco@gmail.com>
2024-03-18 19:33:34 -0500

    make profile a class component and renders users displayName

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,41 +1,54 @@
 import React from 'react';
 import axios from 'axios';
 
-function Profile() {
-  let displayName;
-  const getUser = () => {
-    axios.get('/profile/1')
-      .then((userResponse) => {
-        console.log('client side user response', userResponse);
-        displayName = userResponse.data.displayName;
-        console.log(displayName);
-      })
-      .catch((err) => console.error('Failed getting user data', err));
+class Profile extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      displayName: 'User',
+    }
+
+    this.getUser = () => {
+      axios.get('/profile/1')
+        .then((userResponse) => {
+          const { displayName } = userResponse.data;
+
+          this.setState({ displayName });
+        })
+        .catch((err) => console.error('Failed getting user data', err));
+    }
+
+  };
+
+  componentDidMount() {
+    this.getUser();
   }
 
-  getUser();
-
-  return (
-    <div>
-      <h3>
-        User Profile {displayName}
-        {/* should have name, friends, and when joined */}
-      </h3>
+  render() {
+    const { displayName } = this.state;
+    return (
       <div>
-        User concoctions
-        <ul>
-          <li>Boozy Hibiscus Tea</li>
-        </ul>
+        <h3>
+          User Profile { displayName }
+          {/* should have name, friends, and when joined */}
+        </h3>
+        <div>
+          User concoctions
+          <ul>
+            <li>Boozy Hibiscus Tea</li>
+          </ul>
+        </div>
+        <div>
+          Favorite drinks
+          <ul>
+            <li>Bushwacker</li>
+            <li>French 75</li>
+          </ul>
+        </div>
       </div>
-      <div>
-        Favorite drinks
-        <ul>
-          <li>Bushwacker</li>
-          <li>French 75</li>
-        </ul>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Profile;
