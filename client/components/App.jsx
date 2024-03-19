b1b9bb056e6ba168e1f2df5a7a91ff53ab1835e4
student <student@Carins-MacBook-Pro.local>
2024-03-19 14:36:26 -0500

    added creationStation

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Profile from  './Profile.jsx'
 import Login from './Login.jsx';
 import NotFound from './NotFound.jsx'
+import CreationStation from './CreationStation.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <nav>
       <ul>
         <li><Link to="/profile">User Prof</Link></li>
+        <li><Link to="/creationStation">Creation Station</Link></li>
         
 
       </ul>
@@ -19,6 +21,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<h1>Dash</h1>} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/creationStation" element={<CreationStation />} />
           <Route path="*" element={<NotFound />} />
       </Routes>
   </>
