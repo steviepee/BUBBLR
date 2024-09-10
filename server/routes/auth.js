fea13ade57187e4d8263d89bac73fdfce809359c
mothroom <dakotaday1997@gmail.com>
2024-09-10 18:40:36 -0500

    BUGFIX / Fixed routing bug for passport login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,79 +1,64 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const session = require('express-session');
 const { User } = require('../db/index');
 require('dotenv').config();
 
 const router = express.Router();
-// const app = express();
 
 const { GOOGLE_CLIENT_SECRET, GOOGLE_CLIENT_ID, npm_lifecycle_event } = process.env;
 
-
 const devOrProd = () => {
-  if(npm_lifecycle_event === 'start'){
+  // eslint-disable-next-line camelcase
+  if (npm_lifecycle_event === 'start') {
     return 'localhost';
-  } else {
-    return '13.52.61.243';
   }
-}
+  return '13.52.61.243';
+};
 
-passport.use(new GoogleStrategy(
-  {
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://${devOrProd()}:8080/auth/google/callback`,
-  },
-  ((accessToken, refreshToken, profile, cb) => {
-    // console.log(profile);
-    // console.log('access token: ', accessToken);
-    // console.log('refresh token: ', refreshToken); // undefined
+passport.use(new GoogleStrategy({
+  clientID: `${GOOGLE_CLIENT_ID}`,
+  clientSecret: `${GOOGLE_CLIENT_SECRET}`,
+  callbackURL: `http://${devOrProd()}:8080/auth/google/callback`,
+}, (accessToken, refreshToken, profile, cb) => {
+  console.log(profile)
 
-    // get partners to send there id and displayName to fill out your seed file
-    const { id, displayName } = profile;
-    User.findOrCreate({ where: { googleId: id, displayName } })
-      .then((user) => {
-        cb(null, user);
-      })
-      .catch((err) => cb(err, null));
-  }),
-
-));
-
-passport.serializeUser((user, done) => {
-  // console.log(`--------> Serialize User`);
-  // console.log(user);
-  // console.log(Object.getOwnPropertyNames(user[0]));
-  // even though dataValues is in the above log, the below log shows undefined
-  // console.log(user[0].dataVaules);
-
-  done(null, user);
-  // done(null, user[0].dataVaules.googleId);
-});
-
-passport.deserializeUser((user, done) => {
-// passport.deserializeUser((id, done) => {
-  // console.log("---------> Deserialize User")
-  // console.log(user);
-  // console.log(id);
-  // const user = User.find((u) => u.googleId === id);
-  done(null, user);
-});
-
-router.get('/google', passport.authenticate('google', {
-  scope: ['email', 'profile'],
-}, (req) => {
-  // console.log(req, 'hiiii');
-  req.login((err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
+  User.findOrCreate({
+    where: { googleId: profile.id },
+    defaults: {
+      nameFirst: profile.name.givenName,
+      nameLast: profile.name.familyName,
+      email: profile.emails[0].value,
+    },
+  })
+    .then(([user, created]) => {
+      console.log('User found or created:', user);
+      console.log('Was a new user created?', created);
+      cb(null, user);
+    })
+    .catch((err) => {
+      console.error('Error during user findOrCreate:', err);
+      cb(err, null);
+    });
 }));
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
+
 router.get('/google/callback', passport.authenticate('google', {
-  successRedirect: '/dashboard',
+  successRedirect: '/home',
   failureRedirect: '/login',
 }));
 
