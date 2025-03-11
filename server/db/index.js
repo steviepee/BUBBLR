de8630f49dc1570880a4927342cd866b0c356f76
mydogditto <dhgoldberg15@gmail.com>
2025-03-11 18:55:59 -0500

    FIX changed Achievement data to dat

--- server/db/index.js
+++ server/db/index.js
@@ -195,7 +195,7 @@ const UserAchievements = sequelize.define('UserAchievements', {
       key: 'id',
     },
   },
-  achievementData: {
+  achievementDat: {
     type: DataTypes.INTEGER,
     references: {
       model: Achievements,
