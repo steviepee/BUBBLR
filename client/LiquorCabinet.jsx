2f555f27fee73b8cb9967c771898b1876744099e
mydogditto <dhgoldberg15@gmail.com>
2025-03-12 13:53:22 -0500

    ADD LiquorCabinet component

---
+++ client/LiquorCabinet.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const LiquorCabinet = () => {
+
+  return (
+    <div>
+      <h3>This is your personal Liquor Cabinet</h3>
+    </div>
+  )
+}
+export default LiquorCabinet
