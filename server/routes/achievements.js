4915600bc4c4d540fb6cee081d6c3c03621276f7
khebbler <khebbler@gmail.com>
2025-03-12 13:19:59 -0500

    ADD: GET Achievements by id

--- server/routes/achievements.js
+++ server/routes/achievements.js
@@ -6,12 +6,25 @@ const router = express.Router();
 
 const { Achievements } = require('../db/index');
 
+// get all achievements
 router.get('/', async (req, res) => {
   try {
     const achievements = await Achievements.findAll();
+    // console.log(achievements);
     res.json(achievements);
   } catch (err) {
     console.error('err fetching achievements', err);
+    res.status(500);
+  }
+});
+
+// get achievement by id
+router.get('/:id', async (req, res) => {
+  try {
+    const achievement = await Achievements.findByPk(req.params.id);
+    res.json(achievement);
+  } catch (err) {
+    console.error('err fetching achievement', err);
     res.status(500);
   }
 });
